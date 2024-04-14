2d2ca67967e4d11db892be5a93b9dd420ee3438a
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-14 16:22:13 -0400

    Updated

--- blogapp/src/pages/CreatePost.jsx
+++ blogapp/src/pages/CreatePost.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 const CreatePost = () => {
   return (
-    <div className='CreatePost'>
-      Create Your New Post 
-    </div>
+   <form>
+    <input type='title' placeholder='Title' />
+    <input type='summary' placeholder='Summary' />
+    <input type='file'/>
+    <ReactQuill />
+    <button style={{marginTop: '5px'}}>Upload Post</button>
+   </form>
   )
 }
 
