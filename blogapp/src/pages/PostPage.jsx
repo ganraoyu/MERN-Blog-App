a978d30d5a200313be5dceaac160a220f1041fad
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-18 10:04:59 -0400

    Updated

--- blogapp/src/pages/PostPage.jsx
+++ blogapp/src/pages/PostPage.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import {format} from 'date-fns';
+import { UserContext } from '../UserContext';
+import { Link } from 'react-router-dom';
 
 const PostPage = () => {
     const {id} = useParams();
     const [postInfo, setPostInfo] = useState(null);
+    const {userInfo} = useContext(UserContext);
 
     useEffect(() => {
         fetch(`http://localhost:4000/post/${id}`).then(response => {
@@ -21,6 +24,11 @@ const PostPage = () => {
             <h1>{postInfo.title}</h1>
             <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy, HH:mm')}</time>
             <div className="author">by @{postInfo.author.username} </div>
+            {userInfo.id === postInfo.author._id &&(
+                <div className='edit-row'>
+                    <Link className='edit-btn' to={`/edit/${id}`}>Edit This Post</Link>
+                </div>
+            )}
             <div className='image'>
                 <img  src={`http://localhost:4000/${postInfo.cover}`}/>
                 </div>
