da59755f4af855137c81eb36e43adb9a4ed1127a
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-17 21:09:35 -0400

    Updated

--- blogapp/src/pages/PostPage.jsx
+++ blogapp/src/pages/PostPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import {format} from 'date-fns';
 
 const PostPage = () => {
     const {id} = useParams();
@@ -17,10 +18,12 @@ const PostPage = () => {
     }
     return (
         <div className='post-page'>
+            <h1>{postInfo.title}</h1>
+            <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy, HH:mm')}</time>
+            <div className="author">by @{postInfo.author.username} </div>
             <div className='image'>
                 <img  src={`http://localhost:4000/${postInfo.cover}`}/>
                 </div>
-            <h1>{postInfo.title}</h1>
             <div dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
         </div>
     );
