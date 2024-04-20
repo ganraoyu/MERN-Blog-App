944b7f7c89dc842954140f261258924c2f51662c
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-19 22:06:01 -0400

    Updated

--- api/index.js
+++ api/index.js
@@ -93,16 +93,36 @@ app.post('/post', uploadMiddleware.single('file'), (request, response) => {
     });
 });
  
-app.put('/post', uploadMiddleware.single('file'), async (request, response) => {
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
     let newPath = null;
-    if(request.file){
-        const {originalname, path} = request.file;
-        const parts = originalname.split('.')
-        const extension = parts[parts.length -  1]
-        newPath = path + '.' + extension; 
-        fs.renameSync(path, newPath);
+    if (req.file) {
+      const {originalname,path} = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path+'.'+ext;
+      fs.renameSync(path, newPath);
     }
-});
+  
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info) => {
+      if (err) throw err;
+      const {id,title,summary,content} = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+      await postDoc.update({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+  
+      res.json(postDoc);
+    });
+  
+  });
 
 app.get('/post', async (request, response) => {
     const posts = await Post.find()
--- blogapp/src/pages/EditPost.jsx
+++ blogapp/src/pages/EditPost.jsx
@@ -1,81 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import Editor from '../Editor';
+import {useEffect, useState} from "react";
+import {Navigate, useParams} from "react-router-dom";
+import Editor from "../Editor";
 
-const formats = [
-  'header', 'font', 'size',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'color'
-]
+export default function EditPost() {
+  const {id} = useParams();
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect,setRedirect] = useState(false);
 
-const EditPost = () => {
-    const {id} = useParams()
-    const [title, setTitle] = useState('')
-    const [summary, setSummary] = useState('')
-    const [content, setContent] = useState('')
-    const [files, setFiles] = useState('')
-    const [redirect, setRedirect] = useState(false)
+  useEffect(() => {
+    fetch('http://localhost:4000/post/'+id)
+      .then(response => response.json())
+      .then(postInfo => {
+        setTitle(postInfo.title);
+        setSummary(postInfo.summary);
+        setContent(postInfo.content);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`http://localhost:4000/post/${id}`)
-        .then(response => response.json())
-        .then(postInfo => {
-            setTitle(postInfo.title)
-            setSummary(postInfo.summary)
-            setContent(postInfo.content)
-        })
-    }, [])
-
-    async function updatePost(event){
-        event.preventDefault();
-        const data = new FormData()
-        data.set('title', title)
-        data.set('summary', summary)
-        data.set('content', content)
-        if(files?.[0]){
-            data.set('files', files?.[0])
-        }
-        data.set('files', files?.[0])
-        const response = await fetch('http://localhost:4000/post', {
-            method: 'PUT',
-            body: data,
-
-        })
-        if(response.ok){
-            setRedirect(true)
-        }
-        //setRedirect(true)
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('id', id);
+    if (files?.[0]) {
+      data.set('file', files?.[0]);
     }
-
-    if (redirect){
-        return <Navigate to={'/post/'+id} />
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'PUT',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
     }
-  
-    return (
+  }
+
+  if (redirect) {
+    return <Navigate to={'/post/'+id} />
+  }
+
+  return (
     <>
-        <h1 className='EditPostH1'>Edit Post </h1>
-        <form onSubmit={updatePost}>
-            <input 
-                type='text' 
-                placeholder='Title' 
-                value={title} 
-                onChange={event => setTitle(event.target.value)}
-            />
-            <input 
-                type='text' 
-                placeholder='Summary' 
-                value={summary} 
-                onChange={event => setSummary(event.target.value)}
-            />
-            <input type='file' 
-                onChange={event => setFiles(event.target.files)}
-            />
-            <Editor value={content} onChange={setContent} />
-            <button style={{marginTop: '5px'}}>Edit Post</button>
-        </form>
+      <h1 className='EditPostH1'>Edit Post </h1>
+      <form onSubmit={updatePost}>
+        <input 
+          type='text' 
+          placeholder='Title' 
+          value={title} 
+          onChange={ev => setTitle(ev.target.value)}
+        />
+        <input 
+          type='text' 
+          placeholder='Summary' 
+          value={summary} 
+          onChange={ev => setSummary(ev.target.value)}
+        />
+        <input 
+          type='file' 
+          onChange={ev => setFiles(ev.target.files)}
+        />
+        <Editor onChange={setContent} value={content} />
+        <button style={{marginTop:'5px'}}>Update post</button>
+      </form>
     </>
-    )
+  );
 }
-
-export default EditPost;
