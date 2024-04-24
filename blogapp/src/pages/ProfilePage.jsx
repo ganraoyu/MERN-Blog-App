f8ea5c6c2e0551dc668d222e8e6a9eed330e3133
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-23 22:35:38 -0400

    Updated

--- blogapp/src/pages/ProfilePage.jsx
+++ blogapp/src/pages/ProfilePage.jsx
@@ -3,25 +3,28 @@ import account from '../assets/account.png'
 
 const ProfilePage = () => {
   return (
-    <div className='ProfilePageDiv'>
-      <img src={account} className="profileImage" alt="Account" />
-      <div className="infoStatsDescription">
-        <div className="info">
-          <div>yaoyu</div>
-          <button>Follow</button>
-          <button>Message</button>
+    <>
+      <div className='ProfilePageDiv'>
+        <img src={account} className="profileImage" alt="Account" />
+        <div className="infoStatsDescription">
+          <div className="info">
+            <div>yaoyu</div>
+            <button>Follow</button>
+            <button>Message</button>
+          </div>
+          <div className="stats">
+            <div>0 posts</div>
+            <div>0 followers</div>
+            <div>0 following</div>
+          </div>
+          <div className='description'>
+            Descriptiosdfasdfwefjirwbgowudavnldfijaiodfuhwaeugfwubfiewubgewioyb equhqeiufhiudfiuad f vp eqebf n
+          </div>
         </div>
-        <div className="stats">
-          <div>0 posts</div>
-          <div>0 followers</div>
-          <div>0 following</div>
-        </div>
-        <div className='description'>
-          Descriptiosdfasdfwefjirwbgowudavnldfijaiodfuhwaeugfwubfiewubgewioyb equhqeiufhiudfiuad f vp eqebf n
-        </div>
-        <h1>Blog</h1>
       </div>
-    </div>
+
+      <div><h1>hi</h1></div>
+    </>
   )
 }
 
