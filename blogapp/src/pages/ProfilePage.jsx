ab5279d092d38b687a53140dfbe661008d488c85
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-27 15:29:41 -0400

    updated

--- blogapp/src/pages/ProfilePage.jsx
+++ blogapp/src/pages/ProfilePage.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import account from '../assets/account.png';
 import { useParams } from 'react-router-dom';
 
-const ProfilePage = () => {
+const ProfilePage = ( { _id, title, summary, cover, content, createdAt, author} ) => {
   const { id } = useParams();
   const [profile, setProfile] = useState(null);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:4000/profile/${id}`)
@@ -12,6 +13,16 @@ const ProfilePage = () => {
         return response.json();
       })
       .then(data => setProfile(data))
+      .catch(error => console.error('Error:', error));
+  }, [id]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/posts/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); 
+        setPosts(data);
+      })
       .catch(error => console.error('Error:', error));
   }, [id]);
 
@@ -42,7 +53,9 @@ const ProfilePage = () => {
         <h1 className='h1blog'>Blog</h1>
         <hr/>
         <div className='userBlogPosts'>
-          <img src={account} className="UserProfileImage" alt="Account" />
+        {posts.map(post => (
+  <img key={post._id} src={"http://localhost:4000/" + cover} className="UserPostImage" alt="Post" />
+))}
         </div>
         <div className='blogTitle'>Blog Title</div>
       </div>
